6ad05813db984fed5c03dac51a73b6f1f8624635
knagadevara <vnsk.1991@gmail.com>
2020-04-13 20:55:10 +0530

    Stacked up to getter better Accuracy

--- Project_4/Project4_part2.R
+++ Project_4/Project4_part2.R
@@ -14,7 +14,7 @@ CreateDummies=function(data,var,freq_cutoff=0){
   t=t[t>freq_cutoff]
   t=sort(t)
   categories=names(t)[-1]
-  
+
   for( cat in categories){
     name=paste(var,cat,sep="_")
     name=gsub(" ","",name)
@@ -73,7 +73,8 @@ mutate(
   time_spend_company = unlist(as.factor(time_spend_company)),
   Work_accident = unlist(as.factor(Work_accident)),
   promotion_last_5years = unlist(as.factor(promotion_last_5years))
-  )
+)
+
 Complete_DataSet = CreateDummies(Complete_DataSet,'number_project',100)
 Complete_DataSet = CreateDummies(Complete_DataSet,'time_spend_company',60)
 Complete_DataSet = CreateDummies(Complete_DataSet,'sales',100)
@@ -96,7 +97,6 @@ Final_train1$left = as.factor(Final_train1$left)
 levels(Final_train1$left)
 Final_test1$left = ''
 
-
 ## GLM Model
 model1 = glm(left ~ . -time_spend_company_3 -number_project_4 -time_spend_company_2  , Final_train1 , family = 'binomial')
 val.score = predict(model1,newdata = Final_train1 , type = 'response')
@@ -113,7 +113,6 @@ val_score_test = predict(model1,newdata = Final_test1 , type = 'response')
 fitted_test_results <- ifelse(val_score_test > 0.409,1,0)
 TestGLM_left = fitted_test_results
 
-
 ### Model 2
 library("randomForest")
 set.seed(889)
@@ -129,7 +128,6 @@ RF_MOD1$RF1_left = pred1_train
 pred1_test = predict(rf_model_train1,Final_test1)
 TestRF1_left = pred1_test
 
-
 ### MOdel 3
 ### Training through caret Model = XGBoosting
 ### To check what models are available to names(getModelInfo())
@@ -144,14 +142,12 @@ Final_test_lable = as.factor(Final_test1$left)
 Final_testMat = sparse.model.matrix(left ~ . -left, data = Final_test1 )
 Final_StoreMatrix_Test = xgb.DMatrix(data = as.matrix(Final_testMat) , label = Final_test_lable)
 
-
 ControlParameters_XG = trainControl(method = 'cv' ,
                                     number = 8,
                                     allowParallel = TRUE,
                                     verboseIter = FALSE,
                                     returnData = FALSE
 )
-
 
 parameterGrid_XG <- expand.grid(nrounds = c(100,200,300),  # this is n_estimators in the python code above
                                 max_depth = c(10, 15, 20, 25,30),
@@ -163,7 +159,6 @@ parameterGrid_XG <- expand.grid(nrounds = c(100,200,300),  # this is n_estimator
                                 subsample = 1
 )
 
-
 ### XGB Model
 XG_Model = train(Final_StoreMatrix_Train , Final_train_lable ,
                      method = 'xgbTree' ,
@@ -171,10 +166,8 @@ XG_Model = train(Final_StoreMatrix_Train , Final_train_lable ,
                      tuneGrid = parameterGrid_XG
 ) 
 
-
 predTrain_final_XG = predict(XG_Model,Final_StoreMatrix_Train)
 confusionMatrix(predTrain_final_XG, Final_train1$left)
-
 
 predTest_final_XG = predict(XG_Model,Final_StoreMatrix_Test)
 TestXGB_left = predTest_final_XG
@@ -215,3 +208,32 @@ predTest_Stack = predict(Stack_XG_Model,Stack_StoreMatrix_Test)
 
 # Writing this shit to a damn file
 write.csv(predTest_Stack, 'SaiKarthik_StackingModel.csv', row.names = F)
+
+#### Extra Testing Stack
+Stack_training_Set$TEST_STCK_left = predTrain_StackTrain 
+FGLM = glm(left ~ . -time_spend_company_3 -number_project_4 -time_spend_company_2  , Stack_training_Set , family = 'binomial')
+falscore = predict(FGLM,newdata = Stack_training_Set , type = 'response')
+fauc_score=auc(roc(Stack_training_Set$left , falscore))
+fauc_score
+fitted.results_st <- ifelse(falscore > 0.409,1,0)
+Stack_training_Set$FGLM_DATA = fitted.results_st
+
+## Predicting the data in Testing set and saving them in testing dataframe
+Stack_testME$TEST_STCK_left = predTest_Stack
+val_score_test = predict(FGLM,newdata = Stack_testME , type = 'response')
+StKfitted_test_results <- ifelse(val_score_test > 0.409,1,0)
+Stack_testME$FGLM_DATA = StKfitted_test_results
+
+### Random Forest
+rf_stack_trainM = randomForest(left ~ . -time_spend_company_3 -number_project_4 -time_spend_company_2  , Stack_training_Set , stepFactor=0.5, improve=1e-5, ntree=500, mtry=10 )
+## Predicting
+Spred1_train = predict(rf_stack_trainM,Stack_training_Set)
+confusionMatrix(Spred1_train, Stack_training_Set$left)
+plot(rf_model_train1)
+# ### Saving the obtained predicted column in a new training data set
+# RF_MOD1 = Loggy_Train
+# RF_MOD1$RF1_left = pred1_train
+## Predicting the data in Testing set and saving them in testing dataframe
+Spred1_test = predict(rf_stack_trainM,Stack_testME)
+
+write.csv(Spred1_test , file = "SaiKarthik_NagadevaraP4P2.csv" , row.names = F)
